80a8b22d8917424966aa3264b6f65c795da5cf22
lelum <lelum.madugalle@gmail.com>
2020-04-13 23:07:31 +1200

    update readme and impr script

--- tests_scripts/validate_brand_listed_count.js
+++ tests_scripts/validate_brand_listed_count.js
@@ -6,7 +6,7 @@ function validateListedCount(brandName){
     trademe.usedMotorsSearch().then((response) => {
         const usedMotors = JSON.parse(response); 
         const count = usedMotors.Subcategories.find(subCategory => subCategory.Name === brandName).Count;
-        if (count === undefined){console.log(`${brandName} listed count : Fail`);}
+        if (count === undefined){console.log(`${brandName} listed count : 0`);}
         else{console.log(`${brandName} listed count : ${count}`);}
      })
 }
